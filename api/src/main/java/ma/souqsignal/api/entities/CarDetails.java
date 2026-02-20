ef645c8271341cf6a4bf3deedb060de1621e2493
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2026-02-20 18:50:10 +0000

    Create CarDetails entity with JOINED inheritance mapping

---
+++ api/src/main/java/ma/souqsignal/api/entities/CarDetails.java
@@ -0,0 +1,20 @@
+package ma.souqsignal.api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "car_details")
+@Getter
+@Setter
+@NoArgsConstructor
+@PrimaryKeyJoinColumn(name = "id_signal")
+public class CarDetails extends MarketSignal {
+    private Long kilometrage;
+    private int modele;
+    private String carburant;
+}
