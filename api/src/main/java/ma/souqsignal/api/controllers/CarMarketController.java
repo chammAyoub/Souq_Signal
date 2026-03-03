801c0de32ba43e088cef19f14c12b9fa97767c3e
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2026-03-03 09:10:38 +0000

    feat: Add CarMarketController for car market operations
    - Create CarMarketController with endpoints for saving car signals and fetching arbitrage opportunities.

---
+++ api/src/main/java/ma/souqsignal/api/controllers/CarMarketController.java
@@ -0,0 +1,33 @@
+package ma.souqsignal.api.controllers;
+
+import lombok.RequiredArgsConstructor;
+import ma.souqsignal.api.entities.CarDetails;
+import ma.souqsignal.api.services.CarMarketService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/cars")
+@RequiredArgsConstructor
+public class CarMarketController {
+    private final CarMarketService carMarketService;
+
+    @PostMapping("/sauvegarder")
+    public ResponseEntity<CarDetails> saveCarSignal(@RequestBody CarDetails cardetails){
+        CarDetails saveCarDetails = carMarketService.saveCarSignal(cardetails);
+        return new ResponseEntity<>(saveCarDetails, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/opportunites")
+    public ResponseEntity<List<CarDetails>> getOpportunites(@RequestParam String marque, @RequestParam String modele, @RequestParam Integer annee){
+        List<CarDetails> opportunites = carMarketService.getOpportunitesArbitrage(marque,modele,annee);
+        if(!opportunites.isEmpty()){
+            return new ResponseEntity<>(opportunites, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+}
